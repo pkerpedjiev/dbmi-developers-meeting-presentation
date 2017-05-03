b58c3b4a0e6eb4dba0fa0b5f71aade5e0148954a
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-05-02 22:19:49 -0400

    Added a few more examples

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function drawThreeCircles(divId) {
+
     var width = 550, height=400;
 
     // create an SVG element
@@ -7,9 +8,11 @@ function drawThreeCircles(divId) {
                 .attr('width', width)
                 .attr('height', height)
 
+    var gMain = svg.append('g');
+
     let points = [10,50,20,70];
 
-    var circles = svg.selectAll('circle')
+    var circles = gMain.selectAll('circle')
        .data(points)
        .enter()
        .append('circle')
@@ -42,6 +45,28 @@ function drawThreeCircles(divId) {
         .on('end', moveToRandom);
     }
 
-    moveToRandom();
+    //moveToRandom();
+    /*
+    let zoom = d3.zoom()
+        .on('zoom', () => {
+            gMain.attr('transform', d3.event.transform);   
+        });
+
+    svg.call(zoom)
+    */
+
+    let gBrush = svg.append('g');
+    let brush = d3.brush();
+
+    gBrush.call(brush);
+
+    let scale = d3.scaleLinear().domain([234, 567]).range([20,400])
+
+    scale.ticks();
+    console.log('scale.ticks():', scale.ticks());
+
+    let c = d3.color('Turquoise')
+    console.log('color', c)
+
 }
 
